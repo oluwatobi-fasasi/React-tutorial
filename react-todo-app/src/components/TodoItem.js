44434efb198f4a1970b7c462ff02b64b4f440b68
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-20 11:55:30 -0400

    Create todo app

---
+++ react-todo-app/src/components/TodoItem.js
@@ -0,0 +1,58 @@
+import styles from "../styles/TodoItem.module.css";
+import { useState, useRef } from "react";
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+  const editInputRef = useRef(null);
+  const [editing, setEditing] = useState(false);
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+  const handleEditing = () => {
+    setEditing(true);
+  };
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+  const handleUpdatedDone = (event) => {
+    if (event.key === "Enter") {
+      setUpdate(editInputRef.current.value, itemProp.id);
+      setEditing(false);
+    }
+  };
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          ref={editInputRef}
+          defaultValue={itemProp.title}
+          className={styles.textInput}
+          style={editMode}
+          onKeyDown={handleUpdatedDone}
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
+      <input
+        type="text"
+        value={itemProp.title}
+        className={styles.textInput}
+        style={editMode}
+        onChange={(e) => setUpdate(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+export default TodoItem;
